c0ee30d9bae45dfc10973bac39086084e8a37919
ArmyKetchum <84431612+ArmyKetchum@users.noreply.github.com>
2024-12-27 12:12:34 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,26 @@
+class switchcase{
+	public static void main(String[] args){
+		char ch='a';
+		switch(ch){
+			case 'a':
+				switch(ch){
+					case 'a':
+						System.out.println("vowel");
+						break;
+					case 'e':
+						System.out.println("vowel);
+						break;
+					case 'i':
+						System.out.println("vowel);
+						break;
+					case 'o':
+						System.out.println("vowel);
+						break;
+					case 'u':
+						System.out.println("vowel);
+						break;
+					default:
+						System.out.println("Consonant");
+				}
+		}
+	}

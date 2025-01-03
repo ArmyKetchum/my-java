dbfba61f9b267eb0eb4a5cf69f81e13ff98f3d4d
ArmyKetchum <sunkesulaameer68@gmail.com>
2025-01-03 12:27:41 +0530

    added student.java

---
+++ elseifladder.java
@@ -0,0 +1,12 @@
+public class elseifladder{
+    public static void main(String []args){
+        int marks=65;
+        if(marks<35){
+            System.out.println("fail");
+        }
+        else if (marks>=50 && marks<60){
+            System.out.println("D grade");
+        }
+        else if (marks>)
+    }
+}
---
+++ student.java
@@ -0,0 +1,20 @@
+public class student {
+    int id;
+    String name;
+
+    student(int id, String name) {
+        id = id;
+        name = name;
+    }
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String[] args) {
+        student s1 = new student(101, "Rama");
+        student s2 = new student(102, "Sita");
+        s1.display();
+        s2.display();
+    }
+}

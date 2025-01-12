57398a9fd9ad7923a622f979294210504c4095e3
ArmyKetchum <sunkesulaameer68@gmail.com>
2025-01-12 12:42:39 +0530

    modified

--- arithmetic.java
+++ arithmetic.java
@@ -1,10 +1,10 @@
-class arithmetic{
-public static void main(String args[]){
-int a = 3, b = 3;
-System.out.println("a + b = " + (a + b));
-System.out.println("a - b = " + (a - b));
-System.out.println("a * b = " + (a * b));
-System.out.println("a / b = " + (a / b));
-System.out.println("a % b = " + (a % b));
-}
+class arithmetic {
+    public static void main(String args[]) {
+        int a = 3, b = 3;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
 }
